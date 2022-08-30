2763d1b53ebaf9de58c130791f783954805131b9
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-08-30 08:46:51 -0700

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,7 @@
 import { galleryItems } from './gallery-items.js';
-import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+// import SimpleLightbox from 'simplelightbox';
 import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
-
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 
 
