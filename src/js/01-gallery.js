e5762cdf50ab6c42c102774995672be3d2bb39f0
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-08-30 11:04:07 -0700

    task -1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,64 +1,26 @@
 import { galleryItems } from './gallery-items.js';
-// import SimpleLightbox from 'simplelightbox';
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 
 
+//*    Unpacking   galleryItems      
 
-
-//*    Unpacking   galleryItems
-
-const imagesGalleryContainer = document.querySelector(".gallery");
-const cardsMarkup = (element) => {
-  return `
-  <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="${element.preview}"
-      data-source="${element.original}"
-      alt="${element.description}"
-    />
-  </a>
-</div>`;
+const imagesSwitchGalleryContainer  = document.querySelector(".gallery");
+const cardsSwitchMarkup = element => {
+  return ` <a class="gallery__item gallery__link" href="${element.original}">
+  <img class="gallery__image" src="${element.preview}" alt="${element.description}" class ='gallery__image'/>
+</a>`;
 };
 
-const createListElements = galleryItems.map(cardsMarkup).join("");
-console.log(createListElements);
-imagesGalleryContainer.insertAdjacentHTML("beforeend", createListElements);
+  const createSwitchListElements = galleryItems.map(cardsSwitchMarkup).join('');
+  console.log(createSwitchListElements);
+  imagesSwitchGalleryContainer.insertAdjacentHTML('beforeend', createSwitchListElements);
 
-let instance = null;
+//*   Create   SimpleLightbox
 
-//*   Open  by  click   and create instance
-
-imagesGalleryContainer.addEventListener("click", onPaletteContainerClick);
-
-window.removeEventListener("keydown", onKeyPress);
-function onPaletteContainerClick(evt) {
-  evt.preventDefault();
-
-  if (!evt.target.classList.contains("gallery__image")) {
-    return;
-  }
-  const isCurrentImage = evt.target.dataset.source;
-
-  instance = basicLightbox.create(
-    `<img class = "modal_image" src="${isCurrentImage}"/>`
-  );
-  instance.show();
-  window.addEventListener("keydown", onKeyPress);
-}
-
-//*   Close  Modal  Window
-function onKeyPress(evt) {
-  const ESC_KEY_CODE = "Escape";
-  const isKeyCode = evt.code === ESC_KEY_CODE;
-
-  if (isKeyCode) {
-    instance.close();
-  }
-}
-
-
-// console.log(galleryItems);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
