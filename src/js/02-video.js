14c5ce4c35f3f5e604e327d0ce21e38a9d3d789b
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-09-02 13:59:29 -0700

    Task 3- finished, task 2-fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,31 +1,20 @@
 import throttle from 'lodash.throttle';
-// import Player from '@vimeo/player';
+import Player from '@vimeo/player';
 
+const PLAYER_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-
-
+// *Вешаем throttle
 player.on('timeupdate', throttle(updatePosition, 1000));
 
-function updatePosition ({seconds}){
-  localStorage.setItem('videoplayer-current-time', seconds);
+// *Сохраняем в локал сторадж
+function updatePosition (evt){
+  localStorage.setItem(PLAYER_KEY, JSON.stringify(evt.seconds));
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
-
-
-
-// * @param {number} currentTime
-// * @return {SetCurrentTimePromise}
-// */
-// setCurrentTime(currentTime) {
-//    return this.set('currentTime', currentTime);
-// }
-
-// function setVideoTime(player, seconds) {
-// 	player.setCurrentTime(seconds).then(function() {
-// 		return player.play();
-// 	});
-// }
+// *Вытягиваем данные из  локал сторадж(продолжаем видео с остановки на секундах)
+const savedVideoStorage = localStorage.getItem(PLAYER_KEY);
+if (savedVideoStorage) {
+  player.setCurrentTime(savedVideoStorage);
+}; 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,42 +1,48 @@
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const inputStorage = {};
 const refs = {
   inputFormFeedback: document.querySelector('.feedback-form'),
   inputFormEmail: document.querySelector('[type="email"]'),
   inputFormMessage: document.querySelector('[name="message"]'),
   inputFormSubmitBtn: document.querySelector('[type="submit"]'),
 };
-const inputStorage = {};
 
 refs.inputFormFeedback.addEventListener('submit', onFormSubmit);
-refs.inputFormFeedback.addEventListener(
-  'input',
-  throttle(updateStorageInput, 500)
-);
+refs.inputFormFeedback.addEventListener('input', throttle(onInputStorage, 500));
+populateTextarea();
 
-function updateStorageInput(elt) {
-  inputStorage[elt.target.name] = elt.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(inputStorage));
+// * convert to string and safe to storage_key
+function onInputStorage(e) {
+  inputStorage[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(inputStorage));
 }
 
-function onFormSubmit(event) {
-  event.preventDefault();
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-  const formElements = event.currentTarget.elements;
+// * work with button submit
+function onFormSubmit(evt) {
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  // * 1 add prevent default
+  evt.preventDefault();
+
+// *  check if some input filled if not warning
+  const formElements = evt.currentTarget.elements;
   const emailValue = formElements.email.value;
   const messageValue = formElements.message.value;
-
   if (emailValue === '' || messageValue === '') {
     alert('Все поля ввода должны быть заполнены. Исправьте, пожалуйста.');
-  }
+  };
 
-  event.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
-}
+// *  clear all fields
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+};
 
-// CheckLocalStorage
-
-
-// function isCheckLocalStorage(){
-//   const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-// }
+// *  get from local storage and receive variables to names
+function populateTextarea() {
+  const savedStorageInputs = localStorage.getItem(STORAGE_KEY);
+  if (savedStorageInputs) {
+    refs.inputFormEmail.value = JSON.parse(savedStorageInputs).email;
+    refs.inputFormMessage.value = JSON.parse(savedStorageInputs).message;
+  };
+};
